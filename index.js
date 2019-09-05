f3a4f9b9419067892a0e5929dae01c2e3f4d46ef
github username <github email address>
2019-09-05 14:13:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 function genRandomArr(size) {
-  const arr = new Array(size)
-  for (let idx = 0; idx < size; idx++)
-    arr[idx] = idx + 1
+  const arr = new Array(size);
+  for (let idx = 0; idx < size; idx++) arr[idx] = idx + 1;
 
   // shuffle
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (size))
-    let temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+    const j = Math.floor(Math.random() * size);
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-  return arr
+  return arr;
 }
-
+// adding stuff
 function genWatchedArrays(sorts, cassette) {
-  const baseArray = genRandomArr(config.arrSize)
+  const baseArray = genRandomArr(config.arrSize);
 
   return sorts.map(wSort => {
-    const arrCopy = baseArray.slice()
-    return new WatchedArray(arrCopy, wSort.func, wSort.id, cassette)
-  })
+    const arrCopy = baseArray.slice();
+    return new WatchedArray(arrCopy, wSort.func, wSort.id, cassette);
+  });
 }
 
 function main() {
-  const cassette = new Cassette()
-  const watchedArrays = genWatchedArrays(config.sorts, cassette)
+  const cassette = new Cassette();
+  const watchedArrays = genWatchedArrays(config.sorts, cassette);
   watchedArrays.forEach(arr => {
-    arr.runSort()
-    arr.displayBenchmark()
-  })
-  cassette.play()
+    arr.runSort();
+    arr.displayBenchmark();
+  });
+  cassette.play();
 }
 
-main()
+main();
